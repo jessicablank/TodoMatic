a6988547811f00bcd26b3a8c907e75de824d8a64
Jessica Blankemeier <jessicablankemeier@gmail.com>
2020-10-08 12:48:23 -0700

    Updates

---
+++ precache-manifest.86511208019b8cec17ced3459018d561.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "010edaaac80042a0c88201f7bb34f9be",
+    "url": "/TodoMatic/index.html"
+  },
+  {
+    "revision": "c37562c60f473490f0c6",
+    "url": "/TodoMatic/static/css/main.db85cad5.chunk.css"
+  },
+  {
+    "revision": "6a69c9531ee6f84894db",
+    "url": "/TodoMatic/static/js/2.1e3e62ef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TodoMatic/static/js/2.1e3e62ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c37562c60f473490f0c6",
+    "url": "/TodoMatic/static/js/main.4062d714.chunk.js"
+  },
+  {
+    "revision": "ab07cac62d22ab4be77f",
+    "url": "/TodoMatic/static/js/runtime-main.f63a3a17.js"
+  }
+]);
